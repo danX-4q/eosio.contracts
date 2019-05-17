0e4f1690a3c562565ed5da93a2017b9da769275a
danX-4q <chenxd@bankledger.com>
2019-05-17 18:32:29 +0800

    add [action]feed2 and [table]receive, for testing 2 nodoes sync result when one run success with updatinig table and another run failed

--- safe.chain/include/safe.chain/safe.chain.hpp
+++ safe.chain/include/safe.chain/safe.chain.hpp
@@ -6,8 +6,12 @@
 
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/safechain.h>
+#include <eosiolib/asset.hpp>
+#include <eosiolib/singleton.hpp>
 
 #include <string>
+
+#include "printkit.hpp"
 
 /*namespace eosiosystem {
    class system_contract;
@@ -20,12 +24,42 @@ namespace eosio {
    class [[eosio::contract("safe.chain")]] safechain : public contract {
       public:
          using contract::contract;
+         typedef eosio::name         name;
+         typedef eosio::asset        asset;
+         typedef eosio::datastream<const char*>   datastream__const_char;
+         typedef std::string         string;
+
+         safechain( name receiver, name code,  datastream__const_char ds );
 
          [[eosio::action]]
          void feed( string   txid );
 
+         [[eosio::action]]
+         void feed2( name r_account, string org_chain, string txid );
+
       private:
 
+         struct [[eosio::table]] receive {
+            uint64_t          id;
+            name              receiver;
+            string            txchain;
+            string            txid;
+            checksum256       xtxid;
+
+            uint64_t primary_key() const
+            {
+               return (id);
+            }
+
+            checksum256 get_xtxid() const
+            {
+               return (xtxid);
+            }
+         };
+
+         typedef eosio::multi_index<"receive"_n, receive, 
+            indexed_by<"xtxid"_n, const_mem_fun<receive, checksum256, &receive::get_xtxid>>
+        > type_table__receive;
    };
 
 } /// namespace eosio
--- safe.chain/src/safe.chain.cpp
+++ safe.chain/src/safe.chain.cpp
@@ -7,6 +7,14 @@
 
 namespace eosio {
 
+safechain::safechain(name receiver, name code,  datastream<const char*> ds): 
+    contract(receiver, code, ds)
+{
+    DEBUG_PRINT_VAR(receiver);
+    DEBUG_PRINT_VAR(code);
+    DEBUG_PRINT_VAR(this->get_self());
+}
+
 void safechain::feed( string   txid )
 {
     //require_auth( _self );
@@ -23,6 +31,35 @@ void safechain::feed( string   txid )
     }
 }
 
+void safechain::feed2( name r_account, string txchain, string txid )
+{
+    //require_auth( r_account );
+    eosio_assert( txid.size() == 64, "txid is not 64 bytes" );
+    DEBUG_PRINT_VAR( txid );
+
+    uint64_t confirmations = 0;
+    int ret = get_txid_confirmations(txid.c_str(), txid.size(), confirmations);
+    DEBUG_PRINT_VAR( ret );
+    DEBUG_PRINT_VAR( confirmations );
+    eosio_assert( ret == 0, "error when call get_txid_confirmations" );
+
+    ///////////////////////////////////////////////////////
+
+    checksum256 xtxid = sha256(txid.c_str(), txid.size());
+    type_table__receive tbl_receive(get_self(), "global"_n.value);
+    auto xtxid_index = tbl_receive.get_index<"xtxid"_n>();
+    auto itr_find_xtx = xtxid_index.lower_bound(xtxid);
+    eosio_assert( itr_find_xtx == xtxid_index.end(), "error, txid has been recerved once." );
+
+    tbl_receive.emplace(get_self(), [&]( auto& row ) {
+        row.id = tbl_receive.available_primary_key();
+        row.receiver = r_account;
+        row.txchain = txchain;
+        row.xtxid = xtxid;
+        row.txid = txid;
+    });
+}
+
 } /// namespace eosio
 
-EOSIO_DISPATCH( eosio::safechain, (feed) )
+EOSIO_DISPATCH( eosio::safechain, (feed)(feed2) )
