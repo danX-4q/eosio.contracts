ffc165afb767c4e5e97b5aa96cd8a66085bfc262
danX-4q <chenxd@bankledger.com>
2019-06-20 16:58:00 +0800

    add: exit 1 when build failed.

--- build.sh
+++ build.sh
@@ -7,13 +7,13 @@ NC='\033[0m'
 
 CORES=`getconf _NPROCESSORS_ONLN`
 if [ -d build ]; then
-	pushd build &> /dev/null
-	make -j${CORES} || { popd &> /dev/null; rm -rf build; exit 1; }
-	popd &> /dev/null
+	pushd build &> /dev/null &&
+	make -j${CORES} &&
+	popd &> /dev/null || { exit 1; }
 else 
-	mkdir -p build
-	pushd build &> /dev/null
-	cmake ../
-	make -j${CORES}
-	popd &> /dev/null
+	mkdir -p build &&
+	pushd build &> /dev/null &&
+	cmake ../ &&
+	make -j${CORES} &&
+	popd &> /dev/null || { exit 1; }
 fi
